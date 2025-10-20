ccd79d1fdbb24557b592f256390e7c073ad26464
4teen <meet4teen@gmail.com>
2025-10-20 19:16:42 +1100

    setup

---
+++ src/main/java/pronounce/controllers/HomeController.java
@@ -0,0 +1,20 @@
+package pronounce.controllers;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class HomeController {
+
+    @GetMapping
+    public ResponseEntity<String> home(HttpServletRequest request) {
+        String sessionId = request.getSession().getId();
+        log.info("[{}] testing application 1, 2, 3 ....", sessionId);
+
+        return ResponseEntity.ok("Welcome to pronounce+");
+    }
+}
